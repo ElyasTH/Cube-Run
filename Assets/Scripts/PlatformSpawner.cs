5b3235e2d0dc115938bcd788611c7754340bb104
Elyas Tashakkori <87807482+ElyasTH@users.noreply.github.com>
2024-05-27 12:35:36 +0330

    Revert "-session 3,4"
    This reverts commit f03903bd2d8f533079a7717634ca908a353f3350.

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -8,38 +8,17 @@ public class PlatformSpawner : MonoBehaviour
     [SerializeField] private float spawnTime = 1.5f;
     private float elapsedTime = 0f;
     [SerializeField] private float platformLength = 24f;
-    [SerializeField] private GameObject spikeTrap;
-    [SerializeField] private float spikeTrapHeight = 0.32f;
-    
     private void Update()
     {
         elapsedTime += Time.deltaTime;
         
         if (elapsedTime >= spawnTime)
         {
-            var newPlatform = Instantiate(platform, new Vector3(transform.position.x, transform.position.y, transform.position.z + platformLength),
+            Instantiate(platform, new Vector3(transform.position.x, transform.position.y, transform.position.z + platformLength),
                 Quaternion.identity);
             transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + platformLength);
             
-            SetupTraps(newPlatform.transform);
-            
             elapsedTime = 0;
         }
-    }
-
-    private void SetupTraps(Transform newPlatform)
-    {
-
-        for (int i = 0; i < newPlatform.childCount; i++)
-        {
-            if (i % 4 == 0)
-            {
-                var randomTile = newPlatform.GetChild(i).GetChild(Random.Range(0, 3));
-                Instantiate(spikeTrap,
-                    new Vector3(randomTile.position.x, randomTile.position.y + spikeTrapHeight, randomTile.position.z),
-                    Quaternion.identity);
-            }
-        }
-        
     }
 }
