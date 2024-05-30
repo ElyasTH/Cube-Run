3f8bbf3d92a2301b7e8432aa73225184aae69868
f.galaxy <75903639+fatemehzarebahari@users.noreply.github.com>
2024-05-30 21:08:13 +0330

    adding menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,7 +10,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float scoreIncrementTime;
     [SerializeField] private SoundManager soundManager;
 
+    [Header("Menu")]
+    [SerializeField] private GameObject menu;
+    [SerializeField] private TextMeshProUGUI menuScoreText;
+    [SerializeField] private TextMeshProUGUI menuHighScoreText;
+
+
     private int score;
+    private int highScore;
     private float _elapsedTime;
 
     private bool isGameFinished;
@@ -32,5 +40,35 @@ public class GameManager : MonoBehaviour
         isGameFinished = true;
         soundManager.PLAY_DEATH_SOUND();
         soundManager.STOP_MUSIC();
+
+        SaveHighScore();
+        StartCoroutine(ShowMenu());
+    }
+
+    private IEnumerator ShowMenu()
+    {
+        yield return new WaitForSeconds(2);
+        menuScoreText.text = score.ToString();
+        menuHighScoreText.text = highScore.ToString();
+        menu.SetActive(true);
+    }
+
+    private void SaveHighScore()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            int hs = PlayerPrefs.GetInt("HighScore");
+            if (score > hs)
+                PlayerPrefs.SetInt("HighScore", score);
+        }
+        else
+            PlayerPrefs.SetInt("HighScore", score);
+
+        highScore = PlayerPrefs.GetInt("HighScore");
+    }
+    
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
